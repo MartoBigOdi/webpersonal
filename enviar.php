ab15b13377b92d6d27dca029ea806555439f9842
Marto <martin.vasconcelo@gmail.com>
2020-10-15 23:54:47 -0300

    update mailing

--- enviar.php
+++ enviar.php
@@ -5,23 +5,23 @@ $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
+//Datos para el correo
+$destinatario = "martin.vasconcelo@gmail.com";
+$asunto = 'Mensaje de potencial cliente MV Marketing ';
+
 $header = 'From: ' . $mail . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
 $header .= "Mime-Version: 1.0 \r\n";
 $header .= "Content-Type: text/plain";
 
+//Armando el mensaje
 $message = "Este mensaje fue enviado por: " . $name . " \r\n";
 $message .= "Su e-mail es: " . $mail . " \r\n";
 $message .= "Teléfono de contacto: " . $phone . " \r\n";
 $message .= "Mensaje: " . $_POST['message'] . " \r\n";
 $message .= "Enviado el: " . date('d/m/Y', time());
 
-$para = 'martin.vasconcelo@gmail.com';
-$asunto = 'Mensaje de potencial cliente MV Marketing ';
 
-mail($para, $asunto, utf8_decode($message), $header);
-
+//Enviando el mensaje
+mail($destinatario, $asunto, $message);
 header("Location:index.html");
-
-
-?>
